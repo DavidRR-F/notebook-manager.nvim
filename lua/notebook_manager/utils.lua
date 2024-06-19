8abe7af9716191aa9322483ccc9da4abee0c0454
DavidRR-F <david.rosefranklin96@gmail.com>
2024-06-19 08:43:01 -0400

    ensure notebook directory exists

--- lua/notebook_manager/utils.lua
+++ lua/notebook_manager/utils.lua
@@ -1,6 +1,18 @@
+local uv = vim.loop
+local Path = require('plenary.path')
+local config = require('notebook_manager.config')
 local M = {}
 
+local function ensure_directory_exists(dir)
+  local path = Path:new(dir)
+  if not path:exists() then
+    path:mkdir({ parents = true })
+  end
+end
+
 M.create_book = function(file_name)
+  local file_path = config.options.dir .. "/" .. file_name .. ".ipynb"
+  ensure_directory_exists(config.options.dir)
   print("Creating book: " .. file_name)
 end
 
