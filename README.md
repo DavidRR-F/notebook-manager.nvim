5780e0ce6c675eed75194d4a37fd3a8510c0aa26
DavidRR-F <david.rosefranklin96@gmail.com>
2024-09-16 21:33:41 -0400

    base styling

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+```lua
+extensions = {
+  ...,
+  kernel_manager = {
+    layout_config = {
+        height = 0.5,
+        preview_width = 0.4,
+    },
+  }
+}
+```
--- lua/telescope/_extensions/kernel_manager.lua
+++ lua/telescope/_extensions/kernel_manager.lua
@@ -1,6 +1,9 @@
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
+local previewers = require("telescope.previewers")
+local utils = require("telescope.previewers.utils")
 local config = require("telescope.config").values
+local themes = require('telescope.themes')
 local Job = require("plenary.job")
 local log = require("plenary.log"):new()
 log.level = 'info'
@@ -34,8 +37,21 @@ local function get_kernels()
   return results
 end
 
+M.config = {}
+
+M.setup = function(ext_config)
+  M.config = ext_config or {}
+end
+
 M.show_kernels = function(opts)
   opts = opts or {}
+
+  opts = vim.tbl_deep_extend("force", M.config, opts)
+
+  -- Apply the theme if specified
+  if opts.theme then
+    opts = themes["get_" .. opts.theme](opts)
+  end
 
   pickers.new(opts, {
     prompt_title = "Jupyter Kernels",
@@ -43,18 +59,38 @@ M.show_kernels = function(opts)
       results = get_kernels(),
       entry_maker = function(entry)
         return {
-          value = entry.name,
+          value = entry.resource_dir,
           display = string.format("%s (%s)", entry.display_name, entry.resource_dir),
           ordinal = entry.name,
         }
       end,
     }),
     sorter = config.generic_sorter(opts),
+    previewer = previewers.new_buffer_previewer({
+      title = "Kernel Details",
+      define_preview = function(self, entry)
+        local kernel_json_path = entry.value .. "/kernel.json"
+        local file = io.open(kernel_json_path, "r")
+        if file then
+          -- Read the entire file content
+          local content = file:read("*a")
+          file:close()
+
+          -- Display the content in the preview buffer
+          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, vim.split(content, "\n"))
+        else
+          -- If the file doesn't exist or couldn't be opened, show an error message
+          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, { "Error: Could not open kernel.json" })
+        end      
+        utils.highlighter(self.state.bufnr, "json")
+      end
+    })
   }):find()
 end
 
 return require('telescope').register_extension({
   exports = {
     show_kernels = M.show_kernels
-  }
+  },
+  setup = M.setup,
 })
